848ca47bcf1218bb90fd01814c85c2305f7f59b2
Nailson Israel <nailson.ivs@codens.com.br>
2021-11-03 08:39:44 -0300

    remove images, icons, codes

--- blog.php
+++ blog.php
@@ -1,3 +1,67 @@
 <?php /* Template Name: Blog externa */?>
 <?php get_header(); ?>
+                <form  method="GET">
+                    <div class="div-form-search" role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+                        <input type="text" placeholder="Pesquisar" name="search" value="" class="search-input"/>
+                        <button type="submit" class="btn-sub">
+                            <i class="fas fa-search"></i>
+                        </button>
+                    </div>                    
+                </form>
+            <?php
+            $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
+                $args = new WP_Query([
+                    'post_type'			=> 'post',
+                    'post_status'		=> 'publish',
+                    'orderby'			=> 'rand',
+                    'posts_per_page'    => '6',
+                    'paged' => $paged,
+                    's' => $_GET['search'],
+                ]);
+                add_filter( 'posts_where', 'title_filter', 10, 2 );
+                $query = new WP_Query($args);
+                remove_filter( 'posts_where', 'title_filter', 10, 2 );                
+                if ( $args->have_posts() ) : while ( $args->have_posts() ) : $args->the_post();
+            ?>   
+            <div class="col-12 col-md-6 col-img-blog-img">
+                <a href="<?php the_permalink() ?>" target="_blank" class="">
+                    <img src="<?php echo get_field('banner_article')['url'] ?>" alt="<?php echo the_field('basico_preview')['alt']; ?>" title="<?php echo get_field('banner_article')['title']; ?> " class="img-associetade-blog">           
+                    <li class="category_blog blog-associetade">
+                        <?php
+                        $id = get_the_ID();
+                        $cats = get_the_category($id);
+                        echo $cats[0]->name;
+                        ?>
+                    </li>                
+                    <h1 class="title-associetade-blog"><?php the_title() ?></h1>      
+                    <p class="introduction-associetade-blog"><?php echo excerpt(15) ?></p>
+                    <a href="" class="leiamais">LEIA MAIS</a>
+                </a>                                
+            </div>
+            <?php endwhile; endif; wp_reset_postdata(); ?>  
+    <?php
+        $total_pages = $args->max_num_pages;
+
+            if ($total_pages > 1){
+
+                $current_page = max(1, get_query_var('paged'));
+    ?>
+    <div class="container linkpc">
+        <div class="linkpage">
+        <?php
+                    echo paginate_links(array(
+                        'base' => get_pagenum_link(1) . '%_%',
+                        'format' => '/page/%#%',
+                        'current' => $current_page,
+                        'total' => $total_pages,
+                        'prev_text'    => __('«'),
+                        'next_text'    => __('»'),
+                    ));
+        ?>
+        </div>
+    </div>
+    <?php
+            }            
+    ?>    
 <?php get_footer(); ?>
--- header.php
+++ header.php
@@ -21,7 +21,7 @@ add_action('wp_enqueue_scripts', 'fmg_theme_assets');
             <link rel="stylesheet" href="<?php echo get_bloginfo('template_url') ?>/assets/splide/css/splide-core.min.css">
             <link rel="stylesheet" href="<?php echo get_bloginfo('template_url') ?>/assets/fontawesome/fontawesome-free-6.0.0-beta2-web/css/all.css"/>
             <script>
-                document.addEventListener( 'DOMContentLoaded', function () {
+                /*document.addEventListener( 'DOMContentLoaded', function () {
                     var splide = new Splide( '#slidebanner', {
                         type   : 'loop',
                         focus  : 'left',
@@ -29,103 +29,10 @@ add_action('wp_enqueue_scripts', 'fmg_theme_assets');
                     } );
 
                     splide.mount();
-                } );
+                } );*/
             </script>
             <?php wp_head(); ?>
         </head>
         <body <?php body_class() ?> data-url="<?php echo get_site_url(); ?>">
             <header>
-                <div class="container container-header-mobile">
-                    <div class="row containermenutop">
-                        <div class="col-6 col-md-2">
-                            <a class="menutop" href="tel:(41) 3018-0646">
-                                <i class="fas fa-phone-alt iconstop"></i> (41) 3018-0646
-                            </a>
-                        </div>
-                        <div class="col-6 col-md-2">
-                            <a href="tel:(41) 98497-5483" class="menutop"><i class="fab fa-whatsapp iconstop"></i> (41) 98497-5483</a>
-                        </div>
-                        <div class="col-md-4">
-                            <p class="menutop address"><i class="fas fa-map-marker-alt iconstop"></i> AV. DO BATEL, 1230 TORRE B, SALA 901 - CURITIBA-PR</p>
-                        </div>
-                        <div class="col-12 col-md-2">
-                            <div class="container social">
-                                <a href=""><i class="fab fa-whatsapp iconstop"></i></a>
-                                <a href=""><i class="fab fa-facebook iconstop"></i></a>
-                            </div>
-                        </div>                                                
-                    </div>
-                </div>
-                <div class="container">
-                    <nav class="navbar navbar-expand-lg navbar-light bg-light menu-topo">
-                        <div class="container-fluid">
-                            <a class="navbar-brand" href="<?php echo site_url() ?>">
-                                <img src="<?php echo get_bloginfo('template_url') ?>/assets/images/icons/logo.png" class="logo" alt="" title="">
-                            </a>
-                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                            </button>
-                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav me-auto mb-6 mb-lg-0">
-                                <li class="nav-item">
-                                    <a class="nav-link menu-principal" aria-current="page" href="<?php echo site_url() ?>">HOME</a>
-                                    <?php
-                                        if($_SERVER['REQUEST_URI'] == '/'){
-                                    ?>
-                                        <div class="active-menu"></div>
-                                    <?php
-                                        }
-                                    ?>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link menu-principal" href="<?php the_permalink(135) ?>">QUEM SOMOS</a>
-                                    <?php
-                                        if($_SERVER['REQUEST_URI'] == '/quem-somos/'){
-                                    ?>
-                                        <div class="active-menu"></div>
-                                    <?php
-                                        }
-                                    ?>
-                                </li>
-                                <li class="nav-item nav-drop">
-                                    <a class="nav-link menu-principal" href="#" class="dropservice">SERVIÇOS</a>
-                                    <div class="dropservice">
-                                        <ul>
-                                            <a href="<?php echo site_url() ?>/aparelhos-auditivos"><li>Aparelhos auditivos</li></a>
-                                            <a href="<?php echo site_url() ?>/implantes-cocleares"><li>Implantes</li></a>
-                                        </ul>
-                                    </div>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link menu-principal" href="<?php echo site_url() ?>/blog">BLOG</a>
-                                    <?php
-                                        if($_SERVER['REQUEST_URI'] == '/blog/'){
-                                    ?>
-                                        <div class="active-menu"></div>
-                                    <?php
-                                        }
-                                    ?>
-                                </li>
-                               <li class="nav-item">
-                                    <a class="nav-link menu-principal" href="<?php echo site_url() ?>/contato">CONTATOS</a>
-                                    <?php
-                                        if($_SERVER['REQUEST_URI'] == '/contato/'){
-                                    ?>
-                                        <div class="active-menu"></div>
-                                    <?php
-                                        }
-                                    ?>
-                                </li>
-                            </ul>
-                            <form class="d-flex">
-                                <a href="<?php echo site_url() ?>/contato" class="btn bluebutton">
-                                    FALE COM UM
-                                    <br/>
-                                    <b>PROFISSIONAL</b>
-                                    </a>
-                            </form>
-                            </div>
-                        </div>
-                    </nav>
-                </div>
-            </header>
+            </header
